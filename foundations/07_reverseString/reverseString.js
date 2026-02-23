e6803eb61204ee1c38ed6691d2dfd18b97844946
ez47-av <257847801+ez47-av@users.noreply.github.com>
2026-02-23 13:26:33 -0700

    Complete 07_reverseString
    Reversed the string by using a loop to retrieve
    each char of the string, then concatenating to a
    new string, with the char inserted before.
    
    Alternatively, using slice will convert the
    string into an array of characters, where you can
    then use the array reverse method before joining
    it back into a string.

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+// using a loop, splice each character of the string and concat it to a new string 
+// concat the new character BEFORE the new string to reverse
 
+const reverseString = function(string) {
+  let newString = ""
+
+  for (let i = 0 ; i < string.length; i++) {
+    newString = string[i] + newString;
+  }
+
+  return newString;
 };
 
 // Do not edit below this line
