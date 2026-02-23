056a9dd2d7e1427b895b5bd2360b9a946ff5474f
ez47-av <257847801+ez47-av@users.noreply.github.com>
2026-02-23 14:20:02 -0700

    Update removeFromArray to return a copy
    The original removeFromArray modified the original
    array. The new version uses the filter method.
    If an array item is not found in the passed
    arguments that the user wants gone, then it is
    copied to a new array.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,13 +1,18 @@
-const removeFromArray = function(array, ...removeItems) {
-  removeItems.forEach((item) => {
-    while (array.includes(item)) {
-      let i = array.indexOf(item);
-      array.splice(i, 1);
-    }
-  })
+// this one affects the original array
+// const removeFromArray = function(array, ...removeItems) {
+//   removeItems.forEach((item) => {
+//     while (array.includes(item)) {
+//       let i = array.indexOf(item);
+//       array.splice(i, 1);
+//     }
+//   })
 
-  return array;
-};
+//   return array;
+// };
+
+const removeFromArray = function(array, ...removeItems) {
+  return array.filter((arrItem) => !removeItems.includes(arrItem));
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
