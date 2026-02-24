dbdebd4665515e4726ac5f4f473a1ef88f2caaf7
ez47-av <257847801+ez47-av@users.noreply.github.com>
2026-02-24 12:16:00 -0700

    Complete 09_sumAll
    The function first checks if the number is valid,
    then it determines which input is the min and max.
    
    By setting the sum to the min value,
    it skips through one loop as it can start the
    addition from min + 1. Increments by one until
    reaching the max value.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+// initialize the total variable that will be added on
+// using a for loop, start the initial variable  at the minimum provided value, increments by 1
+// add the initial variable to the total variable, stopping when greater than the maximum provided value
 
+
+
+const sumAll = function(num1, num2) {
+  let total, max;
+
+  if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) {
+    return 'ERROR'
+  }
+
+  if (num1 < num2) {
+    total = num1; //start from the lowest value
+    max = num2;
+  } else {
+    total = num2;
+    max = num1;
+  }
+
+  for (let i = total + 1; i <= max ; i++) {
+    total += i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
