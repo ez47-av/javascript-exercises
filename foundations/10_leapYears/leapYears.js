ca45fcc978d5d83d6d59c24935ebcd95a41d35e7
ez47-av <257847801+ez47-av@users.noreply.github.com>
2026-02-24 12:38:07 -0700

    Complete 10_leapYears
    The math to determine if it is a leap is as
    follows: if the year is divisible by 4 and also
    not by 100, it is a leap year. If it is
    divisible by 100 (meaning it is not a leap year),
    BUT is divisible by 400, it is a leap year

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+// Leap years are years divisible by four
+// Years divisible by 100 are not leap years unless they are divisible by 400
+// If the given input is:
+//    divisible by four, 
+//      AND not divisible by 100 OR divisible by 400
 
+
+const leapYears = function(year) {
+  if (year % 4 === 0 && (!(year % 100 === 0) || year % 400 === 0)) {
+    return true;
+  }
+
+  return false;
 };
 
 // Do not edit below this line
