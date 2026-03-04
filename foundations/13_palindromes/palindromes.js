517000ac88c1053e9b36ab3268073530f689efda
ez47-av <257847801+ez47-av@users.noreply.github.com>
2026-03-04 12:58:30 -0700

    Complete 13_palindromes
    The solution is to first remove any punctation
    or spaces from the given string. This was
    achieved with the replace method. The caret ^
    after the square brackets tells it to replace any
    character that is not listed in the brackets.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  // remove all the punctation first as palindrome don't consider punctuation or spaces, then lowercase
+  string = string.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
 
+  console.log(string);
+  let reverse = "";
+
+  for (let i = 0 ; i < string.length; i++) {
+    reverse = string[i] + reverse;
+  }
+
+  return string === reverse ? true : false;
 };
 
 // Do not edit below this line
