ec0ad139c1b25aa60cb02a7c3b13391d2440ac18
ez47-av <257847801+ez47-av@users.noreply.github.com>
2026-03-04 12:39:40 -0700

    Complete 12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, item) => sum + item, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, item) => total * item, 1)
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  // start the sum at 1 so the factorial of 0 still equals 1
+  // this means you can skip the first iteration of the factorial
+  let sum = 1;
+
+	for (let i = 2; i <= num; i++) {
+    sum *= i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
