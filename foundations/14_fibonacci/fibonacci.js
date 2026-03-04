0deccbc04fd32d1c1ae2c0776373ef5e3730b726
ez47-av <257847801+ez47-av@users.noreply.github.com>
2026-03-04 13:19:59 -0700

    Complete 14_fibonacci
    Note that it would be more memory efficient to
    store the previous two numbers in two variables
    instead of a growing array. Worth to keep in mind.

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+// plan: create an array that keeps track of all the numbers, adding the last two index numbers together for the requested number
+// fibonacci sequence starts at 1 for this exercise
 
+const fibonacci = function(num) {
+  // pre define the first two numbers of the sequence, because you cannot find the 1st or 2nd number if there are not 2 numbers beforehand to calculate
+  const array = [1, 1];
+
+  if (+num === 0) {
+    return 0;
+  }
+
+  if (+num < 0) {
+    return "OOPS";
+  }
+
+  for (let i = 2; i < +num; i++) {
+    array.push(array[i-1] + array[i-2])
+  }
+  
+  return array[+num-1];
 };
 
 // Do not edit below this line
