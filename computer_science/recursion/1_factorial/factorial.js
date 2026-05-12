9757f9377a4ed761d9782e18bc20f22a5095298c
ez47-av <257847801+ez47-av@users.noreply.github.com>
2026-05-12 14:22:50 -0600

    complete 1_factorial

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,13 @@
-const factorial = function() {
+const factorial = function(num) {
+  if (!Number.isInteger(num) || num < 0) {
+    return undefined
+  }
 
+  if (num === 0) {
+    return 1;
+  }
+
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
