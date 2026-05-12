b416f81ea02cad6fe182b39d29ec9153768de156
ez47-av <257847801+ez47-av@users.noreply.github.com>
2026-05-12 15:25:41 -0600

    complete 2_recursion

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,5 +1,16 @@
-const contains = function() {
-  
+const contains = function(obj, value) {
+  if (Object.values(obj).includes(value)) {
+    return true;
+  }
+
+  for (const key in obj) {
+    if (typeof obj[key] === 'object' && obj[key] !== null) {
+      const found = contains(obj[key], value);
+      if (found) return found;
+    }
+  }
+
+  return false;
 };
   
 // Do not edit below this line
