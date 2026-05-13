ee934877babaa58da2f92b4c7b6806a9f4a49a92
ez47-av <257847801+ez47-av@users.noreply.github.com>
2026-05-13 16:30:15 -0600

    complete 5_pascal

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,5 +1,19 @@
-const pascal = function() {
-  
+
+const pascal = function(n) {
+  if (n === 1) return [1];
+
+  // recursive call
+  const prevRow = pascal(n - 1);
+  const newRow = [];
+
+  for (let i = 0; i <= prevRow.length; i++) {
+    // take each number and add it with its rightmost neighbor
+    const left = prevRow[i - 1] ?? 0;
+    const right = prevRow[i] ?? 0;
+    newRow.push(left + right)
+  }
+
+  return newRow;
 };
   
 // Do not edit below this line
