37d2d6905ec2d5ea9a82388e95576cf75c09a091
ez47-av <257847801+ez47-av@users.noreply.github.com>
2026-05-13 14:31:07 -0600

    complete 4_permutations

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,5 +1,28 @@
-const permutations = function() {
-  
+const permutations = function(array, n = array.length, output = []) {
+  // if less than 1 the array passed was empty
+  // slice is called or else it pushes a reference to the original array (which will break during heap's algorithm)
+  if (n <= 1) {
+    output.push(array.slice());
+    return output;
+  } else {
+    for (let i = 0; i <= n - 1; i++) {
+      // called first to ensure the first time in the array is added
+      permutations(array, n - 1, output);
+
+      // heap's algorithm
+      if (n % 2 === 0) {
+        const temp = array[i];
+        array[i] = array[n - 1];
+        array[n - 1] = temp;
+      } else {
+        const temp = array[0];
+        array[0] = array[n - 1];
+        array[n - 1] = temp;
+      }
+    }
+  }
+
+  return output;
 };
   
 // Do not edit below this line
